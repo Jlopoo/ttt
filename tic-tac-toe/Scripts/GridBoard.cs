9274c9f3cbe926570739029c16b609309ebb61d6
Jlopoo <jlopoo@cpromptgames.com>
2026-05-20 08:09:42 -0600

    Remade GridBoard.cs to contain a dictionary as opposed to a data table, simpler

--- tic-tac-toe/Scripts/GridBoard.cs
+++ tic-tac-toe/Scripts/GridBoard.cs
@@ -1,41 +1,29 @@
 using Godot;
 using System;
-using System.Data;
+using System.Collections.Generic;
 
 public partial class GridBoard : Control
 {
-	[Export] public Button button1 { get; set; }
-	[Export] public Button button2 { get; set; }
-	[Export] public Button button3 { get; set; }
-	[Export] public Button button4 { get; set; }
-	[Export] public Button button5 { get; set; }
-	[Export] public Button button6 { get; set; }
-	[Export] public Button button7 { get; set; }
-	[Export] public Button button8 { get; set; }
-	[Export] public Button button9 { get; set; }
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		base._Ready();
 
-		var table = new DataTable("Grid_Positions");
-		table.Columns.Add("Position_Number", typeof(int));
-		table.Columns.Add("Button", typeof(Button));
-		table.Columns.Add("Player_Symbol", typeof(string));
+		Dictionary<int, string> grid_positions = new Dictionary<int, string>();
 
-		table.Rows.Add(1, button1, null);
-		table.Rows.Add(2, button2, null);
-		table.Rows.Add(3, button3, null);
-		table.Rows.Add(4, button4, null);
-		table.Rows.Add(5, button5, null);
-		table.Rows.Add(6, button6, null);
-		table.Rows.Add(7, button7, null);
-		table.Rows.Add(8, button8, null);
-		table.Rows.Add(9, button9, null);
+		grid_positions.Add(1, "open");
+		grid_positions.Add(2, "open");
+		grid_positions.Add(3, "open");
+		grid_positions.Add(4, "open");
+		grid_positions.Add(5, "open");
+		grid_positions.Add(6, "open");
+		grid_positions.Add(7, "open");
+		grid_positions.Add(8, "open");
+		grid_positions.Add(9, "open");
 
-		foreach (DataRow row in table.Rows)
+		foreach (var position in grid_positions)
 		{
-			Console.WriteLine(row["Position_Number"] + " " + row["Button"] + " " + row["Player_Symbol"]);
+			GD.Print("Key: " + position.Key + " - Value: " + position.Value);
 		}
 	}
 
